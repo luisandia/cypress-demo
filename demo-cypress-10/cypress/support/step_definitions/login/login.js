6924eb79a6eabefbeaf299631b2cc06b166539ef
Luis Andia <luisandiamamani@gmail.com>
2022-10-15 23:50:49 -0500

    Fix format

--- demo-cypress-10/cypress/support/step_definitions/login/login.js
+++ demo-cypress-10/cypress/support/step_definitions/login/login.js
@@ -1,33 +1,33 @@
-import { Before, Given, When, And, Then } from "cypress-cucumber-preprocessor/steps";
+import { Before, Given, When, And, Then } from 'cypress-cucumber-preprocessor/steps';
 
 let stub; //we use stub because cypress automatically handles the alert.
-
-Before(() => {
-    cy.log("Executing before step");
+describe('WebdriverUniversity Login Page', () => {
+  Before(() => {
+    cy.log('Executing before step');
     stub = cy.stub();
-})
+  });
 
+  Given('I access the WebdriverUniversity Login Portal page', () => {
+    cy.visit('http://www.webdriveruniversity.com/Login-Portal/index.html');
+  });
 
-Given('I access the WebdriverUniversity Login Portal page', () => {
-    cy.visit("http://www.webdriveruniversity.com/Login-Portal/index.html");
-})
+  When('I enter a username {word}', (userName) => {
+    cy.get('#text').type(userName);
+  });
 
-When("I enter a username {word}", (userName) =>{
-    cy.get("#text").type(userName);
-})
+  And('I enter a password {word}', (userName) => {
+    cy.get('#password').type(userName);
+  });
 
-And("I enter a password {word}", (userName) =>{
-    cy.get("#password").type(userName);
-})
-
-And('I click on the login button', () => {
-    cy.get("#login-button").click();
+  And('I click on the login button', () => {
+    cy.get('#login-button').click();
     cy.on('window:alert', stub); //catch the alert
-})
+  });
 
-Then('I should be presented with the following message {word} {word}', (message, message2) => {
-    const expectedMessage = message + " " + message2;
+  Then('I should be presented with the following message {word} {word}', (message, message2) => {
+    const expectedMessage = message + ' ' + message2;
     cy.log(expectedMessage);
     cy.log(stub.getCall(0));
     expect(stub.getCall(0)).to.be.calledWith(expectedMessage);
-})
+  });
+});
